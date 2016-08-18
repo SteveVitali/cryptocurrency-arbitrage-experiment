f876fb3c7e1a7f7f7cdfa7b6a9d519c4e26e7b8d
Steven Vitali <svitali@seas.upenn.edu>
2016-08-17 21:33:17 -0700

    Print out cycles with exchange simulation data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Graph } from 'graphlib';
-import getExchangeRates from './exchange-rates';
-import bellmanFord from './bellman-ford';
+import { getShapeShiftExchangeRates, getCryptonatorExchangeRates } from './exchange-rates';
+import computeNegativeCycles from './bellman-ford';
 
 getExchangeRates()
   .then(exchangeRates => {
@@ -9,6 +9,7 @@ getExchangeRates()
     Object.keys(exchangeRates).forEach(fromKey => {
       Object.keys(exchangeRates[fromKey]).forEach(toKey => {
         graph.setEdge(fromKey, toKey, exchangeRates[fromKey][toKey]);
+        graph.setEdge(toKey, fromKey, 1 / exchangeRates[fromKey][toKey]);
       });
     });
 
@@ -17,10 +18,35 @@ getExchangeRates()
       return {
         u: e.v,
         v: e.w,
-        w: graph.edge(e)
+        w: -Math.log(graph.edge(e))
       };
     });
-    const source = 'BTC';
-    const { distance, predecessor } = bellmanFord(vertices, edges, source);
+    console.log('edges', edges);
+    const source = 'USD';
+    const negativeCycles = computeNegativeCycles(vertices, edges, source);
+    negativeCycles.forEach(cycle => {
+      let amount = 0.01;
+      let str = '[ 1 ' + cycle[0];
+      let deets = '{';
+      for (let i = 0; i < cycle.length - 1; i++) {
+        const edge = {
+          v: cycle[i],
+          w: cycle[i + 1]
+        };
+        let weight = graph.edge(edge);
+        if (edge.v === edge.w) weight = 1;
+        deets += ' 1 ' + edge.v + '=' + weight + ' ' + edge.w + ', ';
+        amount = amount * weight;
+        if (weight === undefined) return;
+        str += ' --> ' + amount + ' ' + edge.w;
+      }
+      deets += '}';
+      str += ' ]';
+      if (amount > 1) {
+        console.log();
+        console.log(deets);
+        console.log(str);
+      }
+    });
   })
   .catch(err => console.log(err));
